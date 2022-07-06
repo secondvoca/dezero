0e4046d9ca361b39f0a524b021a8ec9e2f5de6f5
secondvoca <secondvoca@gmail.com>
2022-07-06 16:37:49 +0900

    220706 lib 16-2
    tanh

--- functions.py
+++ functions.py
@@ -26,3 +26,16 @@ class Cos(Function):
 
 def cos(x):
     return Cos()(x)
+
+
+class Tanh(Function):
+    def forward(self, x):
+        return np.tanh(x)
+
+    def backward(self, gy):
+        y, = self.outputs[0]()
+        return gy * (1 - y * y)
+
+
+def tanh(x):
+    return Tanh()(x)
