539692564422285f6094984e0ad5e888e415e591
secondvoca <secondvoca@gmail.com>
2022-07-07 15:54:11 +0900

    220707 lib 16
    matmul

--- functions.py
+++ functions.py
@@ -1,8 +1,23 @@
-from turtle import forward, shape
 from dezero.core import as_variable
 import numpy as np
 from dezero.core import Function
 from dezero import utils
+
+
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
+
+def matmul(x, W):
+    return MatMul()(x, W)
 
 
 class Sum(Function):
