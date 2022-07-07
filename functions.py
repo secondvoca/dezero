29e2a647faa2ea5b549b9376cd9335cd6e7f7dde
secondvoca <secondvoca@gmail.com>
2022-07-07 16:27:28 +0900

    220707 lib 17
    mse

--- functions.py
+++ functions.py
@@ -4,6 +4,24 @@ from dezero.core import Function
 from dezero import utils
 
 
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / len(diff)
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
+
+
 class MatMul(Function):
     def forward(self, x, W):
         y = x.dot(W)
