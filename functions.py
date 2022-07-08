3f7ea341e1e87eea703fd7c361c12ceff3d06383
secondvoca <secondvoca@gmail.com>
2022-07-08 09:28:32 +0900

    220708 lib 11-1
    update

--- functions.py
+++ functions.py
@@ -20,9 +20,21 @@ def exp(x):
     return Exp()(x)
 
 
-def sigmoid_simple(x):
-    x = as_variable(x)
-    y = 1 / (1 + exp(-x))
+class Sigmoid(Function):
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        # y = 1 / (1 + xp.exp(-x))
+        y = np.tanh(x * 0.5) * 0.5 + 0.5  # Better implementation
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * y * (1 - y)
+        return gx
+
+
+def sigmoid(x):
+    return Sigmoid()(x)
 
 
 class Linear(Function):
