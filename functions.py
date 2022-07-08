b0cbc36b23b0643aca4c17a90ae574a179dc3c6b
secondvoca <secondvoca@gmail.com>
2022-07-08 09:25:32 +0900

    220708 lib 11
    sigmoid

--- functions.py
+++ functions.py
@@ -4,6 +4,56 @@ from dezero.core import Function
 from dezero import utils
 
 
+class Exp(Function):
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = gy * y
+        return gx
+
+
+def exp(x):
+    return Exp()(x)
+
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+    t = matmul(x, W)
+    if b is None:
+        return t
+
+    y = t + b
+    t.data = None
+    return y
+
+
 class MeanSquaredError(Function):
     def forward(self, x0, x1):
         diff = x0 - x1
