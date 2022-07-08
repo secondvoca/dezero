30c146ffbffd88dcdce35730fd5f415d054cb4a9
secondvoca <secondvoca@gmail.com>
2022-07-08 15:07:49 +0900

    220708 lib 15-2
    layer in layer

--- layers.py
+++ layers.py
@@ -17,7 +17,7 @@ class Layer:
         return outputs if len(outputs) > 1 else outputs[0]
 
     def __setattr__(self, name, value):
-        if isinstance(value, Parameter):
+        if isinstance(value, Parameter, Layer):
             self._params.add(name)
         super().__setattr__(name, value)
 
@@ -26,7 +26,12 @@ class Layer:
 
     def params(self):
         for name in self._params:
-            yield self.__dict__[name]
+            obj = self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
 
     def cleargrads(self):
         for param in self.params():
