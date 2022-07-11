747a8ddc4b8c1ab720af4adf40841787c4237c7f
zisei <secondvoca@gmail.com>
2022-07-11 12:27:25 +0900

    220711 lib 12 from dev folder

--- optimizers.py
+++ optimizers.py
@@ -23,6 +23,27 @@ class Optimizer:
         self.hooks.append(f)
 
 
+import numpy as np
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+
+        v = self.vs[v_key]
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
+
+
 class SGD(Optimizer):
     def __init__(self, lr=0.01):
         super().__init__()
