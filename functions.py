33d019b4b9e60729568d61e5d57531561a693a71
zisei <secondvoca@gmail.com>
2022-07-11 12:47:31 +0900

    220711 lib 13 get_item

--- functions.py
+++ functions.py
@@ -4,6 +4,38 @@ from dezero.core import Function
 from dezero import utils
 
 
+class GetItemGrad(Function):
+    def __init__(self, slices, in_shape):
+        self.slices = slices
+        self.in_shape = in_shape
+
+    def forward(self, gy):
+        gx = np.zeros(self.in_shape)
+        np.add.at(gx, self.slices, gy)
+        return gx
+
+    def backward(self, ggx):
+        return get_item(ggx, self.slices)
+
+
+class GetItem(Function):
+    def __init__(self, slices):
+        self.slices = slices
+
+    def forward(self, x):
+        y = x[self.slices]
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        f = GetItemGrad(self.slices, x.shape)
+        return f(gy)
+
+
+def get_item(x, slices):
+    return GetItem(slices)(x)
+
+
 class Exp(Function):
     def forward(self, x):
         # xp = cuda.get_array_module(x)
