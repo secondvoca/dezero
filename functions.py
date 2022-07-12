f811f5e591562d6310070b08b22e385ae9660ac9
zisei <secondvoca@gmail.com>
2022-07-12 10:46:57 +0900

    220712 lib 11-1

--- functions.py
+++ functions.py
@@ -4,6 +4,57 @@ from dezero.core import Function
 from dezero import utils
 
 
+class Log(Function):
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        y = np.log(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy / x
+        return gx
+
+
+def log(x):
+    return Log()(x)
+
+
+
+class Clip(Function):
+    def __init__(self, x_min, x_max):
+        self.x_min = x_min
+        self.x_max = x_max
+
+    def forward(self, x):
+        # xp = cuda.get_array_module(x)
+        y = np.clip(x, self.x_min, self.x_max)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        mask = (x.data >= self.x_min) * (x.data <= self.x_max)
+        gx = gy * mask
+        return gx
+
+
+def clip(x, x_min, x_max):
+    return Clip(x_min, x_max)(x)
+
+
+
+def softmax_cross_entropy_simple(x, t):
+    x, t = as_variable(x), as_variable(t)
+    N = x.shape[0]
+
+    p = softmax_simple(x)
+    p = clip(p, 1e-15, 1.0)
+    log_p = log(p)
+    tlog_p = log_p[np.arange(N), t.data]
+    y = -1 * sum(tlog_p) / N
+    return y
+
+
 def softmax_simple(x, axis=1):
     x = as_variable(x)
     y = exp(x)
